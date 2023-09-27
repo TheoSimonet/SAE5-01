badb5cba492ce68ef60956d476e38a7d77b838fc
bosc0005 <killian.boscher@etudiant.univ-reims.fr>
2023-09-27 11:29:20 +0200

    add group on api

--- src/Entity/Group.php
+++ src/Entity/Group.php
@@ -2,24 +2,58 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Tests\Fixtures\Metadata\Get;
 use App\Repository\GroupRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: GroupRepository::class)]
-#[ORM\Table(name: '`group`')]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new Post(
+            security: "is_granted('ROLE_ADMIN')",
+        ),
+        new Put(
+            security: "is_granted('ROLE_ADMIN') and object.getUser() == user",
+        ),
+        new Patch(
+            security: "is_granted('ROLE_ADMIN') and object.getUser() == user",
+        ),
+        new Delete(
+            security: "is_granted('ROLE_ADMIN') and object.getUser() == user",
+        ),
+        new Put(
+            normalizationContext: ['groups' => ['get_Semester']],
+            denormalizationContext: ['groups' => ['set_Semester']],
+            security: "is_granted('ROLE_USER') and object == user"
+        ),
+        new Patch(
+            normalizationContext: ['groups' => ['get_Semester']],
+            denormalizationContext: ['groups' => ['set_Semester']],
+            security: "is_granted('ROLE_USER') and object == user"
+        ),
+    ]
+)]
 class Group
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['group:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Groups(['group:read', 'group:write'])]
     private ?string $lib = null;
 
     #[ORM\Column(length: 50)]
+    #[Groups(['group:read', 'group:write'])]
     private ?string $type = null;
 
     public function getId(): ?int
@@ -32,6 +66,7 @@ class Group
         return $this->lib;
     }
 
+    #[Groups(['group:write'])]
     public function setLib(string $lib): static
     {
         $this->lib = $lib;
@@ -44,6 +79,7 @@ class Group
         return $this->type;
     }
 
+    #[Groups(['group:write'])]
     public function setType(string $type): static
     {
         $this->type = $type;
