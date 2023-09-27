550b0d5d94e367a66027f708df1c656cc5c74972
simo0157 <theo.simonet@etudiant.univ-reims.fr>
2023-09-27 11:36:36 +0200

    Add UserType i

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,6 +24,33 @@ class UserController extends AbstractController
     {
         return $this->render('user/show.html.twig', [
             'user' => $user,
+        ]);
+    }
+
+    #[Route('/user/{id}/edit', name: 'app_user_edit', requirements: ['id' => '\d+'])]
+    public function edit(ManagerRegistry $doctrine, Request $request, User $user): Response
+    {
+        $form = $this->createForm(userType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setFirstname($form->getData()->getFirstname());
+            $user->setLastname($form->getData()->getLastname());
+            $user->setEmail($form->getData()->getEmail());
+            $user->setAddress($form->getData()->getAddress());
+
+            $em = $doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_user_show', [
+                'id' => $user->getId(),
+            ]);
+        }
+
+        return $this->render('user/_form.html.twig', [
+            'advertisement' => $user,
+            'form' => $form->createView(),
         ]);
     }
 
---
+++ src/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
