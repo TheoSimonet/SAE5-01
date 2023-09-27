acf876da98dcbdff5fd0fe8ccaa135e5c2c776fc
bosc0005 <killian.boscher@etudiant.univ-reims.fr>
2023-09-27 17:06:13 +0200

    error on new for entity periods

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -3,13 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Group;
+use App\Form\GroupType;
 use App\Repository\GroupRepository;
+use Doctrine\ORM\EntityManagerInterface; // Importez l'EntityManager
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GroupController extends AbstractController
 {
+    private $entityManager; // Déclarez une propriété privée pour l'EntityManager
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager; // Injectez l'EntityManager dans le constructeur
+    }
+
     #[Route('/groups', name: 'group_index')]
     public function index(GroupRepository $groupRepository): Response
     {
@@ -27,4 +37,44 @@ class GroupController extends AbstractController
             'group' => $group,
         ]);
     }
+
+    #[Route('/groups/{id}/edit', name: 'group_edit')]
+    public function edit(Request $request, Group $group): Response
+    {
+        $form = $this->createForm(GroupType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush(); // Utilisez l'EntityManager injecté
+
+            return $this->redirectToRoute('group_show', ['id' => $group->getId()]);
+        }
+
+        return $this->render('group/edit.html.twig', [
+            'group' => $group,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/groups/new', name: 'group_new')]
+    public function new(Request $request): Response
+    {
+        $group = new Group();
+        $form = $this->createForm(GroupType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($group);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('group_show', ['id' => $group->getId()]);
+        }
+
+        return $this->render('group/new.html.twig', [
+            'group' => $group,
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
+
