98c6dcc80015230e98476597551c22900939b5b5
simo0157 <theo.simonet@etudiant.univ-reims.fr>
2023-09-27 15:45:58 +0200

    Add connexion form

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,5 +11,6 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         UserFactory::createMany(10);
+        UserFactory::createOne(['email' => 'root@example.com','login'=> 'admin', 'password' => 'admin', 'roles' => ['ROLE_ADMIN'], 'firstname' => 'admin', 'lastname' => 'admin']);
     }
 }
