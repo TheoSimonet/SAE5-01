cda77da7595ee61e3797ef7502900ced37be5083
scho0029 <luc.schoeny@etudiant.univ-reims.fr>
2023-09-28 09:36:28 +0200

    Creation of the modification and creation form

--- src/Controller/PeriodController.php
+++ src/Controller/PeriodController.php
@@ -3,12 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Period;
+use App\Form\PeriodType;
 use App\Repository\PeriodRepository;
-use App\Repository\SemesterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+
 
 class PeriodController extends AbstractController
 {
@@ -17,8 +21,44 @@ class PeriodController extends AbstractController
     {
         $periods = $repository->findAll();
         return $this->render('period/index.html.twig', [
-            'controller_name' => 'SemesterController',
+            'controller_name' => 'PeriodController',
             'periods' => $periods,
+        ]);
+    }
+
+    #[Route('/periods/new', name: 'app_period_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $period = new Period();
+        $form = $this->createForm(PeriodType::class, $period);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($period);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_period');
+        }
+
+        return $this->render('period/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/periods/edit/{id}', name: 'app_period_edit', methods: ['GET', 'POST'])]
+    public function edit(Period $period, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(PeriodType::class, $period);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+            return $this->redirectToRoute('app_period');
+        }
+
+        return $this->render('period/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
