dda5bfce76b978c60678870cea368cbf5cc9fbd7
bous0056 <karim.boussaid@etudiant.univ-reims.fr>
2023-09-29 16:24:09 +0200

    Addition of the post operation for the subject API

--- src/Entity/Subject.php
+++ src/Entity/Subject.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
 use App\Repository\SubjectRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -12,6 +13,9 @@ use Doctrine\ORM\Mapping as ORM;
 #[ApiResource(
     operations: [
         new Get(),
+        new Post(
+            security: "is_granted('ROLE_ADMIN')",
+        ),
         ]
 )]
 class Subject
