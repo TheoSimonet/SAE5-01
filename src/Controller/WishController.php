732070be870e9f5f5c6ed8ccff451fb06dfdef07
Alexis Hermant <alexis.hermant@etudiant.univ-reims.fr>
2023-10-02 14:19:06 +0200

    Adding wish controller, wish form and view

---
+++ src/Controller/WishController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Wish;
+use App\Form\WishType;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WishController extends AbstractController
+{
+    #[Route('/wish', name: 'app_wish')]
+    public function index(WishRepository $repository): Response
+    {
+        $wishes = $repository->findAll();
+
+        return $this->render('wish/index.html.twig', [
+            'wishes' => $wishes,
+        ]);
+    }
+
+    #[Route('/wish/new', name: 'app_wish_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $wish = new Wish();
+        $form = $this->createForm(WishType::class, $wish);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $wish = $form->getData();
+
+            $manager->persist($wish);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_wish');
+        }
+
+        return $this->render('/wish/_form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /*#[Route('/wish/edit/{id}', 'app_wish_edit', methods: ['GET', 'POST'])]
+    public function edit(Wish $wish, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(WishType::class, $wish);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+            return $this->redirectToRoute('app_wish');
+        }
+
+        return $this->render('/wish/_form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    } */
+
+    #[Route('wish/delete/{id}', 'app_wish_delete', methods: ['GET'])]
+    public function delete(Wish $wish, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($wish);
+        $manager->flush();
+        return $this->redirectToRoute('app_wish');
+    }
+
+}
