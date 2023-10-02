16fb6e15b195acf28cf79f3c41013c14f4fc3304
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-10-02 11:02:09 +0200

    Creating SemesterDetail.jsx

---
+++ assets/react/components/SemesterDetail.jsx
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+import {getSemester} from '../services/api';
+import {useRoute} from 'wouter';
+
+function Semester() {
+    const [semester, setSemester] = useState(null);
+    const [, params] = useRoute('/react/semesters/:id');
+
+    useEffect(() => {
+        getSemester(params.id).then((data) => {
+            setSemester(data);
+        });
+    }, []);
+
+    return (
+        <div>
+            {semester === null ? 'Loading...' : (
+                <div>
+                    <h1>{semester.name}</h1>
+                    <p>Start Date: {semester.startDate}</p>
+                    <p>End Date: {semester.endDate}</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Semester;
