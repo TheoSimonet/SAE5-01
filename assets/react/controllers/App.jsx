9cdbe82158a63aaf86ba3a0436843f9e980cbc54
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-10-02 11:02:16 +0200

    Add routes

--- assets/react/controllers/App.jsx
+++ assets/react/controllers/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Link, Route, Router} from 'wouter';
 import Semesterlist from "../components/SemesterList";
+import Semester from "../components/SemesterDetail";
 function App() {
     return (
         <div className="app">
             <nav>
-                <div>React menu</div>
                 <ul>
                     <li><Link href={'/react/semesters'}>Semestres</Link></li>
                 </ul>
@@ -13,6 +13,9 @@ function App() {
             <Router>
                 <Route path="/react/semesters">
                     <Semesterlist/>
+                </Route>
+                <Route path="/react/semesters/:id">
+                    <Semester/>
                 </Route>
             </Router>
         </div>
