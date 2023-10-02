d7226e08d54d5d4bec863504ebd088fdc7392a54
bosc0005 <killian.boscher@etudiant.univ-reims.fr>
2023-10-02 11:19:45 +0200

    making fixtures for year (entity)

--- src/Entity/Year.php
+++ src/Entity/Year.php
@@ -3,19 +3,55 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Tests\Fixtures\Metadata\Get;
+use App\Repository\GroupRepository;
 use App\Repository\YearRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
-#[ORM\Entity(repositoryClass: YearRepository::class)]
-#[ApiResource]
+#[ORM\Entity(repositoryClass: GroupRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new Post(
+            security: "is_granted('ROLE_ADMIN')",
+        ),
+        new Put(
+            security: "is_granted('ROLE_ADMIN') and object.getUser() == user",
+        ),
+        new Patch(
+            security: "is_granted('ROLE_ADMIN') and object.getUser() == user",
+        ),
+        new Delete(
+            security: "is_granted('ROLE_ADMIN') and object.getUser() == user",
+        ),
+        new Put(
+            normalizationContext: ['groups' => ['get_Year']],
+            denormalizationContext: ['groups' => ['set_Year']],
+            security: "is_granted('ROLE_USER') and object == user"
+        ),
+        new Patch(
+            normalizationContext: ['groups' => ['get_Year']],
+            denormalizationContext: ['groups' => ['set_Year']],
+            security: "is_granted('ROLE_USER') and object == user"
+        ),
+    ]
+)]
+#[ORM\Table(name: 'years')]
 class Year
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['year:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 20)]
+    #[Groups(['year:read', 'year:write'])]
     private ?string $season = null;
 
     public function getId(): ?int
@@ -28,6 +64,7 @@ class Year
         return $this->season;
     }
 
+    #[Groups(['year:write'])]
     public function setSeason(string $season): static
     {
         $this->season = $season;
