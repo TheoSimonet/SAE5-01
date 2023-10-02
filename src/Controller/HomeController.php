b49b656e0e111102506a84e6c2ffd77f296e4369
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-10-02 11:01:32 +0200

    Refactoring controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 
     #[Route('/react', name: 'app_react')]
