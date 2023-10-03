180fa2b66472b3379924677316b4421f8d7d29f1
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-10-03 15:45:39 +0200

    Add a component to see user repartition

--- assets/react/controllers/App.jsx
+++ assets/react/controllers/App.jsx
@@ -3,16 +3,22 @@ import {Link, Route, Router} from 'wouter';
 import Semesterlist from "../components/SemesterList";
 import Grouplist from "../components/GroupList";
 import Semester from "../components/SemesterDetail";
+import  Me from "../components/Me";
+import  Repartition from "../components/Repartition";
 import Group from "../components/GroupDetail";
 
 function App() {
     return (
         <div className="app">
-            <nav>
-                <Semesterlist/>
-                <Grouplist/>
-            </nav>
+            <Me/>
+            <Grouplist/>
             <Router>
+                <Route path="/">
+                    <Repartition/>
+                </Route>
+                <Route path="/react/semesters">
+                    <Semesterlist/>
+                </Route>
                 <Route path="/react/semesters/:id">
                     <Semester/>
                 </Route>
