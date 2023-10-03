7338097348b919d3b05f61ff518f60d31ba5fba5
bous0056 <karim.boussaid@etudiant.univ-reims.fr>
2023-10-03 17:02:42 +0200

    modification of the subject fixture to not use dates anymore

--- src/DataFixtures/SubjectFixtures.php
+++ src/DataFixtures/SubjectFixtures.php
@@ -2,39 +2,21 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\SemesterFactory;
 use App\Factory\SubjectFactory;
-use App\Repository\SemesterRepository;
-use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class SubjectFixtures extends Fixture
 {
-    public SemesterRepository $repository;
-    public function __construct(SemesterRepository $repository)
-    {
-        $this->repository = $repository;
-    }
-
     public function load(ObjectManager $manager): void
     {
-        $subject= file_get_contents(__DIR__ . '/data/subject.json',true);
-        $subjects = json_decode($subject,true);
+        SubjectFactory::createOne(['name' => 'Maths', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 60, 'subjectCode' => 'MR205']);
+        SubjectFactory::createOne(['name' => 'Anglais', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 40, 'subjectCode' => 'MR208']);
+        SubjectFactory::createOne(['name' => 'Communication', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 50, 'subjectCode' => 'MR209']);
+        SubjectFactory::createOne(['name' => 'PPP', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 45, 'subjectCode' => 'MR210']);
+        SubjectFactory::createOne(['name' => 'Gestion', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 55, 'subjectCode' => 'MR212']);
+        SubjectFactory::createOne(['name' => 'Eco', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 42, 'subjectCode' => 'MR213']);
 
-        $semesterRepository = $this->repository;
-
-
-        foreach($subjects as $elmt)
-        {
-            SubjectFactory::createOne([
-                'name' => $elmt['name'],
-                'firstWeek' => new \DateTime($elmt['firstWeek']),
-                'lastWeek' => new \DateTime($elmt['lastWeek']),
-                'hoursTotal' => $elmt['hoursTotal'],
-                'subjectCode' => $elmt['subjectCode'],
-                'semester' => $semesterRepository->find($elmt['semester'])
-            ]);
-        }
+        $manager->flush();
     }
 }
