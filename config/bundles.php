0e1c3d7d87463fae722e33a991e4306930ddba3e
Alexis Hermant <alexis.hermant@etudiant.univ-reims.fr>
2023-10-03 11:31:14 +0200

    Easyadmin installation added

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
