97950fd17b7636aa19c3f83365cb774eac2645a2
bous0056 <karim.boussaid@etudiant.univ-reims.fr>
2023-10-04 11:04:06 +0200

    Removal of the fictional fixtures

--- src/DataFixtures/SubjectFixtures.php
+++ src/DataFixtures/SubjectFixtures.php
@@ -10,12 +10,8 @@ class SubjectFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        SubjectFactory::createOne(['name' => 'Maths', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 60, 'subjectCode' => 'MR205']);
-        SubjectFactory::createOne(['name' => 'Anglais', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 40, 'subjectCode' => 'MR208']);
-        SubjectFactory::createOne(['name' => 'Communication', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 50, 'subjectCode' => 'MR209']);
-        SubjectFactory::createOne(['name' => 'PPP', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 45, 'subjectCode' => 'MR210']);
-        SubjectFactory::createOne(['name' => 'Gestion', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 55, 'subjectCode' => 'MR212']);
-        SubjectFactory::createOne(['name' => 'Eco', 'firstWeek' => 32, 'lastWeek' => 1, 'hoursTotal' => 42, 'subjectCode' => 'MR213']);
+        // $product = new Product();
+        // $manager->persist($product);
 
         $manager->flush();
     }
