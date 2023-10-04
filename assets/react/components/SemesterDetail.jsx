2e7a199f0957e229d6ac7fd2bf3e6357bde645fe
simo0157 <theo.simonet@etudiant.univ-reims.fr>
2023-10-04 10:11:18 +0200

    Add layout to the modal

--- assets/react/components/SemesterDetail.jsx
+++ assets/react/components/SemesterDetail.jsx
@@ -9,16 +9,18 @@ function Semester() {
     const [modalIsOpen, setIsOpen] = React.useState(false);
     const customStyles = {
         content: {
+            fontSize: '3em',
             top: '50%',
             left: '50%',
+            width: '50%',
             right: 'auto',
             bottom: 'auto',
             marginRight: '-50%',
             transform: 'translate(-50%, -50%)',
         },
         overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.2)', // Fond semi-transparent
-            zIndex: 1000, // Assure que le modal est au-dessus du reste du contenu
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            zIndex: 1000,
         },
     };
 
@@ -54,13 +56,20 @@ function Semester() {
                                         isOpen={modalIsOpen}
                                         onRequestClose={closeModal}
                                         style={customStyles}
-                                    >
-                                        <h2>Hello</h2>
-                                        <button onClick={closeModal}>close</button>
-                                        <div>I am a modal</div>
+                                    >   <div className="modal">
+                                        <h2 className='modalTitle'>Réserver un cours</h2>
                                         <form>
-                                            <input />
+                                            <p>Nombre de groupe(s) :</p>
+                                            <input type="number" id="entierPositif" name="entierPositif" min="0" step="1" placeholder="0" /><br/>
+                                            <select id="typeCours" name="typeCours">
+                                                <option value="TD">TD (Travaux Dirigés)</option>
+                                                <option value="TP">TP (Travaux Pratiques)</option>
+                                                <option value="CM">CM (Cours Magistraux)</option>
+                                            </select>
                                         </form>
+                                        <button onClick={closeModal}>Fermer</button>
+                                        <button onClick={closeModal}>Valider</button>
+                                        </div>
                                     </Modal>
                                 </div>
 
