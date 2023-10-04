5c50751eff9e424e3b8a816c1c8bb36641ad47e6
simo0157 <theo.simonet@etudiant.univ-reims.fr>
2023-10-04 09:39:11 +0200

    Add Modal to choose a subject

--- assets/react/components/SemesterDetail.jsx
+++ assets/react/components/SemesterDetail.jsx
@@ -1,10 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import {getSemester} from '../services/api';
 import {useRoute} from 'wouter';
+import Modal from 'react-modal';
 
 function Semester() {
     const [semester, setSemester] = useState(null);
     const [, params] = useRoute('/react/semesters/:id');
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+        overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.2)', // Fond semi-transparent
+            zIndex: 1000, // Assure que le modal est au-dessus du reste du contenu
+        },
+    };
 
     useEffect(() => {
         getSemester(params.id).then((data) => {
@@ -13,6 +29,14 @@ function Semester() {
 
         });
     }, [params.id]);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
 
     return (
         <div>
@@ -25,7 +49,19 @@ function Semester() {
                                 <br/><br/>
                                 <p className="groupe">Groupes |</p>
                                 <div className="Postuler-container">
-                                    <button className="Postuler">Postuler</button>
+                                    <button className="Postuler" onClick={openModal}>Postuler</button>
+                                    <Modal
+                                        isOpen={modalIsOpen}
+                                        onRequestClose={closeModal}
+                                        style={customStyles}
+                                    >
+                                        <h2>Hello</h2>
+                                        <button onClick={closeModal}>close</button>
+                                        <div>I am a modal</div>
+                                        <form>
+                                            <input />
+                                        </form>
+                                    </Modal>
                                 </div>
 
                             </li>
