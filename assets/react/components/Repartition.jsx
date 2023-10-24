6336231af81032773247915f91c2dc90dabcea49
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-10-24 10:10:11 +0200

    Updating view

--- assets/react/components/Repartition.jsx
+++ assets/react/components/Repartition.jsx
@@ -1,8 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import "../../styles/repartition.css";
 import {Link, Route, Router} from 'wouter';
+import {fetchWishes} from "../services/api";
 
 function Repartition() {
+    const [wishes, setWishes] = useState([]);
+
+    useEffect(() => {
+        fetchWishes()
+            .then(data => {
+                if (data) {
+                    setWishes(data['hydra:member']);
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching wishes:", error);
+            });
+    }, []);
+
     return (
         <div className="table-container">
             <h2>Répartition de vos heures</h2>
@@ -10,33 +25,22 @@ function Repartition() {
                 <thead>
                 <tr>
                     <th>Cours</th>
-                    <th>Section</th>
-                    <th>Volume</th>
+                    <th>Groupe</th>
                     <th>Modification</th>
                 </tr>
                 </thead>
                 <tbody>
+                {wishes.map(wish => (
+                    <tr key={wish.id}>
+                        <td>{wish.subjectId}</td>
+                        <td>{wish.chosenGroups}</td>
+                        <td>
+                            <button className="modifier-button">Modifier</button>
+                            <button className="supprimer-button">Supprimer</button>
+                        </td>
+                    </tr>
+                ))}
                 <tr>
-                    <td>Cours 1</td>
-                    <td>Section A</td>
-                    <td>30 heures</td>
-                    <td>
-                        <button className="modifier-button">Modifier</button>
-                        <button className="supprimer-button">Supprimer</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Cours 2</td>
-                    <td>Section B</td>
-                    <td>20 heures</td>
-                    <td>
-                        <button className="modifier-button">Modifier</button>
-                        <button className="supprimer-button">Supprimer</button>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td colSpan="3"></td>
                     <td>
                         <Link to="/react/semesters" className="ajouter-button">Ajouter des heures</Link>
                     </td>
