acf4fd5b72291bf21b74e41a5a05081335810c8f
scho0029 <luc.schoeny@etudiant.univ-reims.fr>
2023-10-24 15:48:25 +0200

    modify the table to make it responsive

--- assets/react/components/Repartition.jsx
+++ assets/react/components/Repartition.jsx
@@ -10,33 +10,22 @@ function Repartition() {
                 <thead>
                 <tr>
                     <th>Cours</th>
-                    <th>Section</th>
-                    <th>Volume</th>
+                    <th>Groupes</th>
                     <th>Modification</th>
                 </tr>
                 </thead>
                 <tbody>
+                {wishes.map(wish => (
+                    <tr key={wish.id}>
+                        <td>{wish.subjectName}</td>
+                        <td>{wish.chosenGroups} groupes de {wish.groupName} </td>
+                        <td>
+                            <button className="modifier-button">Modifier</button>
+                            <button className="supprimer-button">Supprimer</button>
+                        </td>
+                    </tr>
+                ))}
                 <tr>
-                    <td>Cours 1</td>
-                    <td>Section A</td>
-                    <td>30 heures</td>
-                    <td>
-                        <button className="modifier-button">Modifier</button>
-                        <button className="supprimer-button">Supprimer</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Cours 2</td>
-                    <td>Section B</td>
-                    <td>20 heures</td>
-                    <td>
-                        <button className="modifier-button">Modifier</button>
-                        <button className="supprimer-button">Supprimer</button>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td colSpan="3"></td>
                     <td>
                         <Link to="/react/semesters" className="ajouter-button">Ajouter des heures</Link>
                     </td>
